7aece2add389aec229e49fa5e6a4ce9175a053ce
lhp9916 <lhp9916@gmail.com>
2019-10-24 11:59:59 +0800

    sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,58 @@ module.exports = {
   base: "/notes/",
   themeConfig: {
     sidebar: [{
-      title: 'docker',
-      children: [
-        '/docker/install'
-      ]
-    }]
+        title: 'Docker',
+        children: [
+          '/docker/install'
+        ]
+      },
+      {
+        title: 'Git',
+        children: [
+          '/git/git-server',
+          'git/tag'
+        ]
+      },
+      {
+        title: 'Web 前端',
+        children: [
+          '/front-end/npm',
+          'front-end/layer',
+          'front-end/gulp-imagemin.md'
+        ]
+      },
+      {
+        title: 'Laravel',
+        children: [
+          '/laravel/event',
+          '/laravel/tinker',
+          '/laravel/packages'
+        ]
+      },
+      {
+        title: 'Linux',
+        children: [
+          '/linux/ab',
+          '/linux/common-config',
+          '/linux/compress',
+          '/linux/file-transfer',
+          '/linux/gnome-shell',
+          '/linux/nodejs-install',
+          '/linux/php-install',
+          '/linux/ss',
+          '/linux/ssh',
+          '/linux/supervisor',
+          '/linux/ubuntu-setup',
+          '/linux/vim'
+        ]
+      },
+      {
+        title: 'Other',
+        children: [
+          '/license/',
+          '/restful/introduction'
+        ]
+      }
+    ]
   }
 }
